94bfef90e568408c70d9188456a185c698584c04
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2024-08-24 22:14:45 +0300

    app-build

---
+++ dist/nodemailer.js
@@ -0,0 +1,42 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = main;
+const dotenv_1 = require("dotenv");
+const promises_1 = require("fs/promises");
+const path_1 = require("path");
+const nodemailer = require("nodemailer");
+(0, dotenv_1.config)();
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.APP_EMAIL_PASSWORD,
+    },
+});
+function main(email) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const htmlTemplate = yield (0, promises_1.readFile)((0, path_1.join)(__dirname, "email.html"), "utf-8");
+        try {
+            const info = yield transporter.sendMail({
+                from: `"Ivan Dev 🧑‍💻" <${process.env.EMAIL_USER}>`,
+                to: email,
+                subject: "Hello ✔",
+                text: "Hello world?",
+                html: htmlTemplate,
+            });
+            console.log("Message sent: %s", info.messageId);
+        }
+        catch (error) {
+            console.error("Error sending email:", error);
+        }
+    });
+}
