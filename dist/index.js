33224c53ff33176e747e423b7263240010c96212
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2024-08-24 23:01:38 +0300

    add

--- dist/index.js
+++ dist/index.js
@@ -21,7 +21,10 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)({
-    origin: "https://nodemailer-app-kappa.vercel.app",
+    origin: [
+        "https://nodemailer-app-kappa.vercel.app",
+        "http://localhost:3000",
+    ],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
 }));
